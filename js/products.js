450e418329ff3fd64351cdcb9e08bab25a6ae06b
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2021-08-17 17:18:52 -0300

    Lista de productos y login v1

--- js/products.js
+++ js/products.js
@@ -2,5 +2,28 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+   
+    function listaProductos(url){
+        fetch(url)
+            .then(respuesta => respuesta.json())
+    
+            .then(datos => {
+                   
+                    for (valor of datos){
+                        document.getElementById("listaProductos").innerHTML += `
+
+                        <p>Precio: ${valor.cost} </p>
+                        <p>Nombre: ${valor.name} </p>
+                        <p>Descripción: ${valor.description} </p>
+                        <hr>
+                        ` 
+                            
+                    }
+    
+            })
+            .catch(error => alert("Hubo un error: " + error));
+    }
+
+    listaProductos(PRODUCTS_URL);
 
 });
