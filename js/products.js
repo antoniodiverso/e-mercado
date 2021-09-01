b656b33a8a2d421e2b1c2141df0e10cb491ef037
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2021-09-01 16:28:46 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,6 +2,9 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+
+    var maxPrecio;
+    var minPrecio;
    
     function listaProductos(url){
         fetch(url)
@@ -10,6 +13,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
             .then(datos => {
                    
                     for (valor of datos){
+
+                        if(((minPrecio == undefined) || (minPrecio != undefined && parseInt(valor.cost)>= minPrecio)) && ((maxPrecio == undefined) || (maxPrecio != undefined && parseInt(valor.cost)<=maxPrecio))){
+
                         document.getElementById("listaProductos").innerHTML += `
 
                         <p>Precio: ${valor.cost} </p>
@@ -19,11 +25,50 @@ document.addEventListener("DOMContentLoaded", function (e) {
                         ` 
                             
                     }
-    
+                    }
             })
             .catch(error => alert("Hubo un error: " + error));
     }
 
     listaProductos(PRODUCTS_URL);
+
+
+    document.getElementById("botonFiltrar").addEventListener("click", function(){
+
+        minPrecio = document.getElementById("minimo").value;
+        maxPrecio = document.getElementById("maximo").value;
+
+        if((minPrecio != undefined) && (minPrecio != "") && (parseInt(minPrecio))>=0 ){
+            minPrecio = parseInt(minPrecio);
+        }
+        else{
+            minPrecio = undefined;
+        }
+
+        if((maxPrecio != undefined) && (maxPrecio != "") && (parseInt(maxPrecio))>=0 ){
+            maxPrecio = parseInt(maxPrecio);
+        }
+        else{
+            maxPrecio = undefined;
+        }
+
+
+        listaProductos(PRODUCTS_URL);
+
+
+    })
+
+    document.getElementById("limpiarFiltro").addEventListener("click", function(){
+
+        minPrecio = document.getElementById("minimo").value="";
+        maxPrecio = document.getElementById("maximo").value="";
+
+
+        minPrecio = undefined;
+        maxPrecio = undefined;
+
+        listaProductos(PRODUCTS_URL);
+    })
+
 
 });
