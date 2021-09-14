0a314610582ab89b92a7acaf30e4919f66935292
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2021-09-14 11:06:24 -0300

    Creando el form de los comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -2,5 +2,16 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+ document.getElementById("estrella1");
+ document.getElementById("estrella2");
+ document.getElementById("estrella3");
+ document.getElementById("estrella4");
+ document.getElementById("estrella5");
+
+});
+
+document.getElementById("estrella1").addEventListener("click", function (){
+
+
 
 });
