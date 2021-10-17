a930ce4b062d773607d61c5790032e4b2a7e9218
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2021-10-17 18:43:20 -0300

    El carrtio es responsive

--- js/cart.js
+++ js/cart.js
@@ -6,59 +6,75 @@ var infoCarrito;
 
 function mostrarCarrito(productoCarrito) {
     let contenidoCarrito = ""
-    let sub = (productoCarrito[0].unitCost) * (productoCarrito[0].count)
     contenidoCarrito += `
-        <div class="container">
-
-        <br>
-
+      
+    <br>
             <div class="row">
-                <div class="col-8" id="productBody">
+                <div class="col-12 col-md-8" id="productBody">
                     <h1>Carrito</h1>
                     <div class="row">
-                        <div class="col-6">Producto</div>
-                        <div class="col">Cantidad</div>
-                        <div class="col">Precio</div>
-                        <div class="col">Subtotal</div>
-
-                    </div>
-                    <div class="row" >
-                        <div class="col-6">
-                            <div class="row">
-                                <div class="col-6"><img src="img/tree1.jpg" width=100% alt=""></div>
-                                <div class="col-6">${productoCarrito[0].name}</div>
-                            </div>
-                        </div>
-                        <div class="col"><input type="number" onchange="calcularSub(${productoCarrito[0].unitCost})" value="${productoCarrito[0].count}" id="numCantidad" name="numCantidad" min="1" style="width: 100%;"></div>
-                        <div class="col">${productoCarrito[0].unitCost} UYU</div>
-                        <div class="col" id="subtotalProductos">${(productoCarrito[0].unitCost) * (productoCarrito[0].count)} UYU</div>
-
+                <div class="col-12 col-lg-6">
+                    <div class="col">Producto</div>
+                <div class="col">
+                <div class="row">
+                        <div class="col-6"><img src="img/tree1.jpg" width=100% alt=""></div>
+                    <div class="col-6">${productoCarrito[0].name}</div>
                     </div>
                 </div>
-                <div class="col-4" id="importeTotal" style="background-color: lightgrey;">
-                    
+                </div>
+
+            <div class="col-12 col-lg-2">
+                <div class="col">Cantidad</div>
+                <div class="col"><input type="number" onkeydown="return false" onchange="calcularSub(${productoCarrito[0].unitCost})" value="${productoCarrito[0].count}" id="numCantidad" name="numCantidad" min="1" style="width: 50px;"></div>
+            </div>
+            
+
+            <div class="col-12 col-lg-2">
+                <div class="col">Precio</div>
+                <div class="col-12">${productoCarrito[0].unitCost}UYU</div>
+            </div>
+
+            <div class="col-12 col-lg-2">
+                <div class="col">Subtotal</div>
+                <div class="col-12" id="subtotalProductos">${(productoCarrito[0].unitCost) * (productoCarrito[0].count)}UYU</div>
+            </div>
+            </div>
+        </div>
+                  
+                
+               
+                <div class="col-12 col-md-4" id="importeTotal" style="background-color: lightgrey;">
+                <h2>Importe total</h2>
+                <div class="col">
                     <div class="row">
-                    <h2>Importe total</h2>
+                    
 
                         <div class="col-6">Total</div>
-                        <div class="col-6" id="totalProductos" >${(productoCarrito[0].unitCost) * (productoCarrito[0].count)} UYU</div>
+                        <div class="col-6" id="totalProductos" >${(productoCarrito[0].unitCost) * (productoCarrito[0].count)}UYU</div>
                         
                         </div>
                         </div>
+                        <br><br>
+                    <div class="col">
+                        <button class="btn btn-success">Proceder con la compra</button>
+                    </div>
+                
+                        </div>
             </div>
+       
 
             <br>
 
-            </div>`
-     
+           `
+
     document.getElementById("contenidoCarrito").innerHTML += contenidoCarrito;
 }
 
-function calcularSub(precioUni){
-let cantidad = parseInt(document.getElementById(`numCantidad`).value);
-subtotal = precioUni * cantidad;
-document.getElementById(`subtotalProductos`).innerHTML = subtotal + " UYU";
-document.getElementById(`totalProductos`).innerHTML = subtotal + " UYU";
+function calcularSub(precioUni) {
+    let cantidad = parseInt(document.getElementById(`numCantidad`).value);
+    subtotal = precioUni * cantidad;
+    document.getElementById(`subtotalProductos`).innerHTML = subtotal + "UYU";
+    document.getElementById(`totalProductos`).innerHTML = subtotal + "UYU";
 }
 
 
